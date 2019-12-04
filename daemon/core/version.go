3e6520edad0dd40b5bb1e25ba582db24cf971648
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2019-12-04 23:16:35 +0100

    Version changed to 1.0.0-rc1

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.0.0b"
+	Version = "1.0.0-rc1"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
