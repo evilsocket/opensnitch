7d330f24d991ecfd68455ad734d609d5e5281e3d
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-02-05 00:57:06 +0100

    Bump version to 1.0.0-rc2

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.0.0-rc1"
+	Version = "1.0.0-rc2"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
