f4845240cc28264636714dc92d357a8db1213835
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-07-29 01:17:05 +0200

    fixed typo and lint errors

--- daemon/core/core.go
+++ daemon/core/core.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"os/user"
@@ -13,10 +12,12 @@ const (
 	defaultTrimSet = "\r\n\t "
 )
 
+// Trim remove trailing spaces from a string.
 func Trim(s string) string {
 	return strings.Trim(s, defaultTrimSet)
 }
 
+// Exec spawns a new process and reurns the output.
 func Exec(executable string, args []string) (string, error) {
 	path, err := exec.LookPath(executable)
 	if err != nil {
@@ -26,11 +27,11 @@ func Exec(executable string, args []string) (string, error) {
 	raw, err := exec.Command(path, args...).CombinedOutput()
 	if err != nil {
 		return "", err
-	} else {
-		return Trim(string(raw)), nil
 	}
+	return Trim(string(raw)), nil
 }
 
+// Exists checks if a path exists.
 func Exists(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false
@@ -38,6 +39,7 @@ func Exists(path string) bool {
 	return true
 }
 
+// ExpandPath replaces '~' shorthand with the user's home directory.
 func ExpandPath(path string) (string, error) {
 	// Check if path is empty
 	if path != "" {
