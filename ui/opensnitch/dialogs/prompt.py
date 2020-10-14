1f6a2db964f930a2ddcae81a31f0b67b9179eea5
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-10-14 23:35:05 +0200

    ui: don't set the focus on the action buttons
    Sometimes you can deny/allow a connection unintentionally while typing.
    Now the focus is set to the "+" button, and you can jump to the deny or
    allow button with the arrow keys.
    
    Issue #58.

--- ui/opensnitch/dialogs/prompt.py
+++ ui/opensnitch/dialogs/prompt.py
@@ -177,12 +177,12 @@ class PromptDialog(QtWidgets.QDialog, uic.loadUiType(DIALOG_UI_PATH)[0]):
         if self._cfg.getSettings(self.CFG_DEFAULT_ACTION) == self.ACTION_ALLOW:
             self.applyButton.setText("%s (%d)" % (self._apply_text, self._tick))
             self.denyButton.setText(self._deny_text)
-            self.applyButton.setFocus()
         else:
             self.denyButton.setText("%s (%d)" % (self._deny_text, self._tick))
             self.applyButton.setText(self._apply_text)
-            self.denyButton.setFocus()
 
+
+        self.checkAdvanced.setFocus()
 
     def _render_connection(self, con):
         app_name, app_icon, _ = self._apps_parser.get_info_by_path(con.process_path, "terminal")
