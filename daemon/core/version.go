c666abe5f90bffcb5ca0fe6ec90156c836f6906d
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2020-11-05 21:50:37 +0100

    apps version increased

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -1,8 +1,9 @@
 package core
 
+// version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.0.1"
+	Version = "1.2.0"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
