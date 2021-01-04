8bc06e50aaa2733d8ee78c38e11b000fcb0194bc
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2021-01-04 21:53:20 +0100

    i18n: updated spanish and basque translations

--- ui/i18n/locales/eu_ES/opensnitch-eu_ES.ts
+++ ui/i18n/locales/eu_ES/opensnitch-eu_ES.ts
@@ -1100,53 +1100,58 @@ You must name the rule in such manner that it&apos;ll be checked first, because 
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="258"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="246"/>
         <source>from this executable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="260"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="248"/>
         <source>from this command line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="222"/>
-        <source>&lt;b&gt;%s&lt;/b&gt; is connecting to &lt;b&gt;%s&lt;/b&gt; on %s port %d</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="266"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="254"/>
         <source>to port {0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="318"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="306"/>
         <source>to {0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="269"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="257"/>
         <source>from user {0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="282"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="270"/>
         <source>to {0}.*</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="328"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="316"/>
         <source>to *.{0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="332"/>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="320"/>
         <source>to *{0}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/prompt.py" line="228"/>
-        <source>&lt;b&gt;Remote&lt;/b&gt; process &lt;b&gt;%s&lt;/b&gt; running on &lt;b&gt;%s&lt;/b&gt; is connecting to &lt;b&gt;%s&lt;/b&gt; on %s port %d</source>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="330"/>
+        <source>&lt;b&gt;Remote&lt;/b&gt; process %s running on &lt;b&gt;%s&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="334"/>
+        <source>is connecting to &lt;b&gt;%s&lt;/b&gt; on %s port %d</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../opensnitch/dialogs/prompt.py" line="340"/>
+        <source>is attempting to resolve &lt;b&gt;%s&lt;/b&gt; via %s, %s port %d</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -1309,12 +1314,12 @@ You must name the rule in such manner that it&apos;ll be checked first, because 
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/stats.py" line="794"/>
+        <location filename="../../../opensnitch/dialogs/stats.py" line="793"/>
         <source>    Your are about to delete this rule.    </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/stats.py" line="796"/>
+        <location filename="../../../opensnitch/dialogs/stats.py" line="795"/>
         <source>    Are you sure?</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1389,7 +1394,7 @@ You must name the rule in such manner that it&apos;ll be checked first, because 
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/stats.py" line="1131"/>
+        <location filename="../../../opensnitch/dialogs/stats.py" line="1130"/>
         <source>Save as CSV</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1399,13 +1404,8 @@ You must name the rule in such manner that it&apos;ll be checked first, because 
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../opensnitch/dialogs/stats.py" line="600"/>
+        <location filename="../../../opensnitch/dialogs/stats.py" line="599"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../opensnitch/dialogs/stats.py" line="571"/>
-        <source>&lt;b&gt;Error:&lt;/b&gt;&lt;br&gt;&lt;br&gt;{0}</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
