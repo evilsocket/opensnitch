8ea58ade19a487d2276f573a9b60de34c5bb0852
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2021-04-05 12:19:00 +0200

    fixed ebpf error logging
    + split functionality into different files.

---
+++ daemon/procmon/ebpf/find.go
@@ -0,0 +1,142 @@
+package ebpf
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"unsafe"
+
+	daemonNetlink "github.com/evilsocket/opensnitch/daemon/netlink"
+)
+
+// we need to manually remove old connections from a bpf map
+
+// GetPid looks up process pid in a bpf map. If not found there, then it searches
+// already-established TCP connections.
+func GetPid(proto string, srcPort uint, srcIP net.IP, dstIP net.IP, dstPort uint) (int, int, error) {
+	if hostByteOrder == nil {
+		return -1, -1, fmt.Errorf("eBPF monitoring method not initialized yet")
+	}
+
+	if pid, uid := getPidFromEbpf(proto, srcPort, srcIP, dstIP, dstPort); pid != -1 {
+		return pid, uid, nil
+	}
+	//check if it comes from already established TCP
+	if proto == "tcp" || proto == "tcp6" {
+		if pid, uid, err := findInAlreadyEstablishedTCP(proto, srcPort, srcIP, dstIP, dstPort); err == nil {
+			return pid, uid, nil
+		}
+	}
+	//using netlink.GetSocketInfo to check if UID is 0 (in-kernel connection)
+	if uid, _ := daemonNetlink.GetSocketInfo(proto, srcIP, srcPort, dstIP, dstPort); uid == 0 {
+		return -100, -100, nil
+	}
+	if !findAddressInLocalAddresses(srcIP) {
+		// systemd-resolved sometimes makes a TCP Fast Open connection to a DNS server (8.8.8.8 on my machine)
+		// and we get a packet here with **source** (not detination!!!) IP 8.8.8.8
+		// Maybe it's an in-kernel response with spoofed IP because wireshark does not show neither
+		// resolved's TCP Fast Open packet, nor the response
+		// Until this is better understood, we simply do not allow this machine to make connections with
+		// arbitrary source IPs
+		return -1, -1, fmt.Errorf("eBPF packet with unknown source IP: %s", srcIP)
+	}
+	return -1, -1, nil
+}
+
+// getPidFromEbpf looks up a connection in bpf map and returns PID if found
+// the lookup keys and values are defined in opensnitch.c , e.g.
+//
+// struct tcp_key_t {
+// 	u16 sport;
+// 	u32 daddr;
+// 	u16 dport;
+//  u32 saddr;
+// }__attribute__((packed));
+
+// struct tcp_value_t{
+// 	u64 pid;
+//  u64 uid;
+// 	u64 counter;
+// }__attribute__((packed));;
+
+func getPidFromEbpf(proto string, srcPort uint, srcIP net.IP, dstIP net.IP, dstPort uint) (pid int, uid int) {
+	var key []byte
+	var value []byte
+	var isIP4 bool = (proto == "tcp") || (proto == "udp") || (proto == "udplite")
+
+	if isIP4 {
+		key = make([]byte, 12)
+		value = make([]byte, 24)
+		copy(key[2:6], dstIP)
+		binary.BigEndian.PutUint16(key[6:8], uint16(dstPort))
+		copy(key[8:12], srcIP)
+	} else { // IPv6
+		key = make([]byte, 36)
+		value = make([]byte, 24)
+		copy(key[2:18], dstIP)
+		binary.BigEndian.PutUint16(key[18:20], uint16(dstPort))
+		copy(key[20:36], srcIP)
+	}
+	hostByteOrder.PutUint16(key[0:2], uint16(srcPort))
+
+	err := m.LookupElement(ebpfMaps[proto].bpfmap, unsafe.Pointer(&key[0]), unsafe.Pointer(&value[0]))
+	if err != nil {
+		// key not found
+		// maybe srcIP is 0.0.0.0 Happens especially with UDP sendto()
+		// TODO: can this happen with TCP?
+		if isIP4 {
+			zeroes := make([]byte, 4)
+			copy(key[8:12], zeroes)
+		} else {
+			zeroes := make([]byte, 16)
+			copy(key[20:36], zeroes)
+		}
+		err = m.LookupElement(ebpfMaps[proto].bpfmap, unsafe.Pointer(&key[0]), unsafe.Pointer(&value[0]))
+	}
+	if err != nil && proto == "udp" && srcIP.String() == dstIP.String() {
+		// very rarely I see this connection. It has srcIP and dstIP == 0.0.0.0 in ebpf map
+		// it is a localhost to localhost connection
+		// srcIP was already set to 0, set dstIP to zero also
+		// TODO try to reproduce it and look for srcIP/dstIP in other kernel structures
+		zeroes := make([]byte, 4)
+		copy(key[2:6], zeroes)
+		err = m.LookupElement(ebpfMaps[proto].bpfmap, unsafe.Pointer(&key[0]), unsafe.Pointer(&value[0]))
+	}
+	if err != nil {
+		// key not found in bpf map
+		return -1, -1
+	}
+	pid = int(hostByteOrder.Uint32(value[0:4]))
+	uid = int(hostByteOrder.Uint32(value[8:12]))
+	return pid, uid
+}
+
+// FindInAlreadyEstablishedTCP searches those TCP connections which were already established at the time
+// when opensnitch started
+func findInAlreadyEstablishedTCP(proto string, srcPort uint, srcIP net.IP, dstIP net.IP, dstPort uint) (int, int, error) {
+	var alreadyEstablished map[*daemonNetlink.Socket]int
+	if proto == "tcp" {
+		alreadyEstablished = alreadyEstablishedTCP
+	} else if proto == "tcp6" {
+		alreadyEstablished = alreadyEstablishedTCPv6
+	}
+	for sock, v := range alreadyEstablished {
+		if (*sock).ID.SourcePort == uint16(srcPort) && (*sock).ID.Source.Equal(srcIP) &&
+			(*sock).ID.Destination.Equal(dstIP) && (*sock).ID.DestinationPort == uint16(dstPort) {
+			return v, int((*sock).UID), nil
+		}
+	}
+	return -1, -1, fmt.Errorf("eBPF inode not found")
+}
+
+//returns true if addr is in the list of this machine's addresses
+func findAddressInLocalAddresses(addr net.IP) bool {
+	localAddressesLock.Lock()
+	defer localAddressesLock.Unlock()
+	for _, a := range localAddresses {
+		if addr.String() == a.String() {
+			return true
+		}
+	}
+	return false
+}
---
+++ daemon/procmon/ebpf/monitor.go
@@ -0,0 +1,170 @@
+package ebpf
+
+import (
+	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/evilsocket/opensnitch/daemon/log"
+	daemonNetlink "github.com/evilsocket/opensnitch/daemon/netlink"
+	elf "github.com/iovisor/gobpf/elf"
+	"github.com/vishvananda/netlink"
+)
+
+// we need to manually remove old connections from a bpf map
+// since when a bpf map is full it doesn't allow any more insertions
+func monitorMaps() {
+	zeroKey := make([]byte, 4)
+	for {
+		time.Sleep(time.Second * 1)
+		if stop {
+			return
+		}
+		for name, ebpfMap := range ebpfMaps {
+			value := make([]byte, 8)
+			if err := m.LookupElement(ebpfMap.counterMap,
+				unsafe.Pointer(&zeroKey[0]), unsafe.Pointer(&value[0])); err != nil {
+				log.Error("eBPF m.LookupElement error: %v", err)
+			}
+			counterValue := hostByteOrder.Uint64(value)
+			if counterValue-ebpfMap.lastPurgedMax > 10000 {
+				ebpfMap.lastPurgedMax = counterValue - 5000
+				deleteOld(ebpfMap.bpfmap, name == "tcp6" || name == "udp6", ebpfMap.lastPurgedMax)
+			}
+		}
+	}
+}
+
+// maintains a list of this machine's local addresses
+func monitorLocalAddresses() {
+	for {
+		addr, err := netlink.AddrList(nil, netlink.FAMILY_ALL)
+		if err != nil {
+			log.Error("eBPF error looking up this machine's addresses via netlink: %v", err)
+			continue
+		}
+		localAddressesLock.Lock()
+		localAddresses = nil
+		for _, a := range addr {
+			localAddresses = append(localAddresses, a.IP)
+		}
+		localAddressesLock.Unlock()
+		time.Sleep(time.Second * 1)
+		if stop {
+			return
+		}
+	}
+}
+
+// monitorAlreadyEstablished makes sure that when an already-established connection is closed
+// it will be removed from alreadyEstablished. If we don't do this and keep the alreadyEstablished entry forever,
+// then after the genuine process quits,a malicious process may reuse PID-srcPort-srcIP-dstPort-dstIP
+func monitorAlreadyEstablished() {
+	for {
+		time.Sleep(time.Second * 1)
+		if stop {
+			return
+		}
+		socketListTCP, err := daemonNetlink.SocketsDump(uint8(syscall.AF_INET), uint8(syscall.IPPROTO_TCP))
+		if err != nil {
+			log.Error("eBPF error in dumping TCP sockets via netlink")
+			continue
+		}
+		for aesock := range alreadyEstablishedTCP {
+			found := false
+			for _, sock := range socketListTCP {
+				if (*aesock).INode == (*sock).INode &&
+					//inodes are unique enough, so the matches below will never have to be checked
+					(*aesock).ID.SourcePort == (*sock).ID.SourcePort &&
+					(*aesock).ID.Source.Equal((*sock).ID.Source) &&
+					(*aesock).ID.Destination.Equal((*sock).ID.Destination) &&
+					(*aesock).ID.DestinationPort == (*sock).ID.DestinationPort &&
+					(*aesock).UID == (*sock).UID {
+					found = true
+					break
+				}
+			}
+			if !found {
+				delete(alreadyEstablishedTCP, aesock)
+			}
+		}
+
+		socketListTCPv6, err := daemonNetlink.SocketsDump(uint8(syscall.AF_INET6), uint8(syscall.IPPROTO_TCP))
+		if err != nil {
+			log.Error("eBPF error in dumping TCPv6 sockets via netlink")
+			continue
+		}
+		for aesock := range alreadyEstablishedTCPv6 {
+			found := false
+			for _, sock := range socketListTCPv6 {
+				if (*aesock).INode == (*sock).INode &&
+					//inodes are unique enough, so the matches below will never have to be checked
+					(*aesock).ID.SourcePort == (*sock).ID.SourcePort &&
+					(*aesock).ID.Source.Equal((*sock).ID.Source) &&
+					(*aesock).ID.Destination.Equal((*sock).ID.Destination) &&
+					(*aesock).ID.DestinationPort == (*sock).ID.DestinationPort &&
+					(*aesock).UID == (*sock).UID {
+					found = true
+					break
+				}
+			}
+			if !found {
+				delete(alreadyEstablishedTCPv6, aesock)
+			}
+		}
+	}
+}
+
+// delete all map's elements which have counter value <= maxToDelete
+func deleteOld(bpfmap *elf.Map, isIPv6 bool, maxToDelete uint64) {
+	var lookupKey []byte
+	var nextKey []byte
+	var value []byte
+	if !isIPv6 {
+		lookupKey = make([]byte, 12)
+		nextKey = make([]byte, 12)
+		value = make([]byte, 24)
+	} else {
+		lookupKey = make([]byte, 36)
+		nextKey = make([]byte, 36)
+		value = make([]byte, 24)
+	}
+	firstrun := true
+	i := 0
+	for {
+		i++
+		if i > 12000 {
+			// there were more iterations than the max amount of elements in map
+			// TODO find out what causes the endless loop
+			// maybe because ebpf prog modified the map while we were iterating
+			log.Error("Breaking because endless loop was detected in deleteOld")
+			break
+		}
+		ok, err := m.LookupNextElement(bpfmap, unsafe.Pointer(&lookupKey[0]),
+			unsafe.Pointer(&nextKey[0]), unsafe.Pointer(&value[0]))
+		if err != nil {
+			log.Error("eBPF LookupNextElement error: %v", err)
+			return
+		}
+		if firstrun {
+			// on first run lookupKey is a dummy, nothing to delete
+			firstrun = false
+			copy(lookupKey, nextKey)
+			continue
+		}
+		// last 8 bytes of value is counter value
+		counterValue := hostByteOrder.Uint64(value[16:24])
+		if counterValue > maxToDelete {
+			copy(lookupKey, nextKey)
+			continue
+		}
+		if err := m.DeleteElement(bpfmap, unsafe.Pointer(&lookupKey[0])); err != nil {
+			log.Error("eBPF DeleteElement error: %v", err)
+			return
+		}
+		if !ok { //reached end of map
+			break
+		}
+		copy(lookupKey, nextKey)
+	}
+}
