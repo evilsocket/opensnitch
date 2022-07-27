82691eb2b0912300273ed601b569c8a7dbe13a3e
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2022-07-27 17:36:47 +0200

    Bump versions to v1.5.2

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -3,7 +3,7 @@ package core
 // version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.5.1"
+	Version = "1.5.2"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
