d055067bafb7a6870340e8a6a32c8027a1b91b3a
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2023-02-05 19:17:36 +0100

    i18n: fixed creating locales on fedora
    kudos to pizzadude for add a note to the wiki:
    3164b857c43226cf068b11364e18065f910ce4d4

--- ui/i18n/generate_i18n.sh
+++ ui/i18n/generate_i18n.sh
@@ -2,11 +2,16 @@
 
 app_name="opensnitch"
 langs_dir="./locales"
+lrelease="lrelease"
+if ! command -v $lrelease; then
+    # fedora
+    lrelease="lrelease-qt5"
+fi
 
 #pylupdate5 opensnitch_i18n.pro
 
 for lang in $(ls $langs_dir)
 do
     lang_path="$langs_dir/$lang/$app_name-$lang"
-    lrelease $lang_path.ts -qm $lang_path.qm
+    $lrelease $lang_path.ts -qm $lang_path.qm
 done
