5cdbd6182a6d6f569664dea9d84ae5df1dca84e4
Spencer Comfort <109806759+GiddyGoatGaming@users.noreply.github.com>
2023-02-05 16:14:45 -0500

    Remove unused parameters (#829)
    * Update highlight.py

--- ui/opensnitch/actions/highlight.py
+++ ui/opensnitch/actions/highlight.py
@@ -141,9 +141,7 @@ class Highlight():
                     style,
                     painter,
                     option,
-                    index,
                     defaultPen,
-                    defaultBrush,
                     cellAlignment,
                     cellRect,
                     cellColor,
@@ -197,7 +195,7 @@ class Highlight():
 
         return (modified,)
 
-    def paintCell(self, style, painter, option, index, defaultPen, defaultBrush, cellAlignment, cellRect, cellColor, cellBgColor, cellValue):
+    def paintCell(self, style, painter, option, defaultPen, cellAlignment, cellRect, cellColor, cellBgColor, cellValue):
         cellSelected = option.state & QStyle.State_Selected
 
         painter.save()
