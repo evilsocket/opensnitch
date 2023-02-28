0c28dbe26a0d33067b806546af74fe3a92bbef6f
NRGLine4Sec <26851996+NRGLine4Sec@users.noreply.github.com>
2023-02-28 20:50:26 +0100

    make version consistent with github tags

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -3,7 +3,7 @@ package core
 // version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.6.0rc5"
+	Version = "1.6.0-rc5"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
