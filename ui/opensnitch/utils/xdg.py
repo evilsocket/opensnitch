0dd3f00859f0bb3b6b9f0e88db83bf6586a0d124
munix9 <44939650+munix9@users.noreply.github.com>
2023-06-09 17:26:16 +0200

    do not use pyxdg (for now), use some code from there

--- ui/opensnitch/utils/xdg.py
+++ ui/opensnitch/utils/xdg.py
@@ -1,12 +1,46 @@
 
 import os
+import re
 import shutil
-import xdg.BaseDirectory
-import xdg.DesktopEntry
 
-xdg_config_home = xdg.BaseDirectory.xdg_config_home
-xdg_runtime_dir = xdg.BaseDirectory.get_runtime_dir(False)
-xdg_current_desktop = os.environ.get('XDG_CURRENT_DESKTOP')
+# https://github.com/takluyver/pyxdg/blob/1d23e483ae869ee9532aca43b133cc43f63626a3/xdg/BaseDirectory.py
+def get_runtime_dir(strict=True):
+    try:
+        return os.environ['XDG_RUNTIME_DIR']
+    except KeyError:
+        if strict:
+            raise
+
+        import getpass
+        fallback = '/tmp/opensnitch-' + getpass.getuser()
+        create = False
+
+        try:
+            # This must be a real directory, not a symlink, so attackers can't
+            # point it elsewhere. So we use lstat to check it.
+            st = os.lstat(fallback)
+        except OSError as e:
+            import errno
+            if e.errno == errno.ENOENT:
+                create = True
+            else:
+                raise
+        else:
+            # The fallback must be a directory
+            if not stat.S_ISDIR(st.st_mode):
+                os.unlink(fallback)
+                create = True
+            # Must be owned by the user and not accessible by anyone else
+            elif (st.st_uid != os.getuid()) \
+              or (st.st_mode & (stat.S_IRWXG | stat.S_IRWXO)):
+                os.rmdir(fallback)
+                create = True
+
+        if create:
+            os.mkdir(fallback, 0o700)
+
+        return fallback
+
 
 class Autostart():
     def __init__(self):
@@ -24,13 +58,17 @@ class Autostart():
             os.makedirs(os.path.dirname(self.userAutostart), 0o755)
 
     def isEnabled(self):
+        ret = False
         if os.path.isfile(self.userAutostart):
-            entry = xdg.DesktopEntry.DesktopEntry(self.userAutostart)
-            if not entry.getHidden():
-                return True
+             ret = True
+             lines = open(self.userAutostart, 'r').readlines()
+             for line in lines:
+                 if re.search("^Hidden=true", line, re.IGNORECASE):
+                     ret = False
+                     break
         elif os.path.isfile(self.systemAutostart):
-            return True
-        return False
+            ret = True
+        return ret
 
     def enable(self, mode=True):
         self.createUserDir()
@@ -49,3 +87,9 @@ class Autostart():
 
     def disable(self):
         self.enable(False)
+
+
+_home = os.path.expanduser('~')
+xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or os.path.join(_home, '.config')
+xdg_runtime_dir = get_runtime_dir(False)
+xdg_current_desktop = os.environ.get('XDG_CURRENT_DESKTOP')
