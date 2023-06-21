59621017e96a6e049f42db1dff609cc2a549cbf4
munix9 <44939650+munix9@users.noreply.github.com>
2023-06-21 23:28:13 +0200

    xdg updates + autostart + lockfile (#964)
    * require pyxdg
    
    * extend xdg, introduce autostart
    
    * use xdg_current_desktop from opensnitch.utils.xdg
    
    * control autostart in tray
    
    * dont use pkill anymore
    
    * check if os-ui is already running
    
    * don't require pyxdg (for now)
    
    * simplify xdg_current_desktop
    
    * do not use pyxdg (for now), use some code from there
    
    * update autostart status when menu is open
    
    * fix possible SameFileError

--- ui/opensnitch/dialogs/stats.py
+++ ui/opensnitch/dialogs/stats.py
@@ -23,6 +23,7 @@ from opensnitch.customwidgets.firewalltableview import FirewallTableModel
 from opensnitch.customwidgets.generictableview import GenericTableModel
 from opensnitch.customwidgets.addresstablemodel import AddressTableModel
 from opensnitch.utils import Message, QuickHelp, AsnDB, Icons
+from opensnitch.utils.xdg import xdg_current_desktop
 from opensnitch.actions import Actions
 from opensnitch.rules import Rule, Rules
 
@@ -276,8 +277,6 @@ class StatsDialog(QtWidgets.QDialog, uic.loadUiType(DIALOG_UI_PATH)[0]):
 
     def __init__(self, parent=None, address=None, db=None, dbname="db", appicon=None):
         super(StatsDialog, self).__init__(parent)
-
-        self._current_desktop = os.environ['XDG_CURRENT_DESKTOP'] if os.environ.get("XDG_CURRENT_DESKTOP") != None else None
 
         self.setWindowFlags(QtCore.Qt.Window)
         self.setupUi(self)
@@ -625,7 +624,7 @@ class StatsDialog(QtWidgets.QDialog, uic.loadUiType(DIALOG_UI_PATH)[0]):
         if event.type() == QtCore.QEvent.WindowStateChange:
             if event.oldState() & QtCore.Qt.WindowMinimized and event.oldState() & QtCore.Qt.WindowMaximized:
                 #a previously minimized maximized window ...
-                if self.windowState() ^ QtCore.Qt.WindowMinimized and self._current_desktop == "KDE":
+                if self.windowState() ^ QtCore.Qt.WindowMinimized and xdg_current_desktop == "KDE":
                     # is not minimized anymore, i.e. it was unminimized
                     # docs: https://doc.qt.io/qt-5/qwidget.html#setWindowState
                     self.setWindowState(self.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
--- ui/opensnitch/utils/xdg.py
+++ ui/opensnitch/utils/xdg.py
@@ -1,5 +1,101 @@
 
 import os
+import re
+import shutil
 
-_user_home = os.path.expanduser('~')
-xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or os.path.join(_user_home, '.config')
+# https://github.com/takluyver/pyxdg/blob/1d23e483ae869ee9532aca43b133cc43f63626a3/xdg/BaseDirectory.py
+def get_runtime_dir(strict=True):
+    try:
+        return os.environ['XDG_RUNTIME_DIR']
+    except KeyError:
+        if strict:
+            raise
+
+        import getpass
+        fallback = '/tmp/opensnitch-' + getpass.getuser()
+        create = False
+
+        try:
+            # This must be a real directory, not a symlink, so attackers can't
+            # point it elsewhere. So we use lstat to check it.
+            st = os.lstat(fallback)
+        except OSError as e:
+            import errno
+            if e.errno == errno.ENOENT:
+                create = True
+            else:
+                raise
+        else:
+            # The fallback must be a directory
+            if not stat.S_ISDIR(st.st_mode):
+                os.unlink(fallback)
+                create = True
+            # Must be owned by the user and not accessible by anyone else
+            elif (st.st_uid != os.getuid()) \
+              or (st.st_mode & (stat.S_IRWXG | stat.S_IRWXO)):
+                os.rmdir(fallback)
+                create = True
+
+        if create:
+            os.mkdir(fallback, 0o700)
+
+        return fallback
+
+
+class Autostart():
+    def __init__(self):
+        desktopFile = 'opensnitch_ui.desktop'
+        self.systemDesktop = os.path.join('/usr/share/applications', desktopFile)
+        self.systemAutostart = os.path.join('/etc/xdg/autostart', desktopFile)
+        if not os.path.isfile(self.systemAutostart) and os.path.isfile('/usr' + self.systemAutostart):
+            self.systemAutostart = '/usr' + self.systemAutostart
+        self.userAutostart = os.path.join(xdg_config_home, 'autostart', desktopFile)
+
+    def createUserDir(self):
+        if not os.path.isdir(xdg_config_home):
+            os.makedirs(xdg_config_home, 0o700)
+        if not os.path.isdir(os.path.dirname(self.userAutostart)):
+            os.makedirs(os.path.dirname(self.userAutostart), 0o755)
+
+    def isEnabled(self):
+        ret = False
+        if os.path.isfile(self.userAutostart):
+             ret = True
+             lines = open(self.userAutostart, 'r').readlines()
+             for line in lines:
+                 if re.search("^Hidden=true", line, re.IGNORECASE):
+                     ret = False
+                     break
+        elif os.path.isfile(self.systemAutostart):
+            ret = True
+        return ret
+
+    def enable(self, mode=True):
+        self.createUserDir()
+        if mode == True:
+            if os.path.isfile(self.systemAutostart) and os.path.isfile(self.userAutostart):
+                os.remove(self.userAutostart)
+            elif os.path.isfile(self.systemDesktop):
+                try:
+                    shutil.copyfile(self.systemDesktop, self.userAutostart)
+                except shutil.SameFileError:
+                    pass
+        else:
+            if os.path.isfile(self.systemAutostart):
+                try:
+                    shutil.copyfile(self.systemAutostart, self.userAutostart)
+                except shutil.SameFileError:
+                    pass
+                with open(self.userAutostart, 'a') as f:
+                    f.write('Hidden=true\n')
+            elif os.path.isfile(self.userAutostart):
+                os.remove(self.userAutostart)
+
+    def disable(self):
+        self.enable(False)
+
+
+_home = os.path.expanduser('~')
+xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or os.path.join(_home, '.config')
+xdg_runtime_dir = get_runtime_dir(False)
+xdg_current_desktop = os.environ.get('XDG_CURRENT_DESKTOP')
