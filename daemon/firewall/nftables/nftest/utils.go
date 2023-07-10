c2e6032a86f4603e8272bbe30914569c752c1549
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2023-07-11 01:16:23 +0200

    tests: added needed helpers

--- daemon/firewall/nftables/nftest/utils.go
+++ daemon/firewall/nftables/nftest/utils.go
@@ -43,3 +43,75 @@ func AddTestRule(t *testing.T, conn *nftables.Conn, exp *[]expr.Any) (*nftables.
 
 	return r, chn
 }
+
+// AddTestSNATRule adds a generic table, chain and rule with the given expression.
+func AddTestSNATRule(t *testing.T, conn *nftables.Conn, exp *[]expr.Any) (*nftables.Rule, *nftables.Chain) {
+
+	_, err := Fw.AddTable("uuu", exprs.NFT_FAMILY_INET)
+	if err != nil {
+		t.Errorf("pre step add_table() uuu-inet failed: %s", err)
+		return nil, nil
+	}
+	chn := Fw.AddChain(
+		exprs.NFT_HOOK_POSTROUTING,
+		"uuu",
+		exprs.NFT_FAMILY_INET,
+		nftables.ChainPriorityNATSource,
+		nftables.ChainTypeNAT,
+		nftables.ChainHookPostrouting,
+		nftables.ChainPolicyAccept)
+	if chn == nil {
+		t.Error("pre step add_chain() input-uuu-inet failed")
+		return nil, nil
+	}
+	//nft.Commit()
+
+	r, err := Fw.AddRule(
+		exprs.NFT_HOOK_POSTROUTING, "uuu", exprs.NFT_FAMILY_INET,
+		0,
+		"key-uuu",
+		exp)
+	if err != nil {
+		t.Errorf("Error adding rule: %s", err)
+		return nil, nil
+	}
+	t.Logf("Rule: %+v", r)
+
+	return r, chn
+}
+
+// AddTestDNATRule adds a generic table, chain and rule with the given expression.
+func AddTestDNATRule(t *testing.T, conn *nftables.Conn, exp *[]expr.Any) (*nftables.Rule, *nftables.Chain) {
+
+	_, err := Fw.AddTable("iii", exprs.NFT_FAMILY_INET)
+	if err != nil {
+		t.Errorf("pre step add_table() iii-inet failed: %s", err)
+		return nil, nil
+	}
+	chn := Fw.AddChain(
+		exprs.NFT_HOOK_PREROUTING,
+		"iii",
+		exprs.NFT_FAMILY_INET,
+		nftables.ChainPriorityNATDest,
+		nftables.ChainTypeNAT,
+		nftables.ChainHookPrerouting,
+		nftables.ChainPolicyAccept)
+	if chn == nil {
+		t.Error("pre step add_chain() input-iii-inet failed")
+		return nil, nil
+	}
+	//nft.Commit()
+
+	r, err := Fw.AddRule(
+		exprs.NFT_HOOK_PREROUTING, "iii", exprs.NFT_FAMILY_INET,
+		0,
+		"key-iii",
+		exp)
+	if err != nil {
+		t.Errorf("Error adding rule: %s", err)
+		return nil, nil
+	}
+	t.Logf("Rule: %+v", r)
+
+	return r, chn
+}
