ce7c3f80024551c0fe1a549abc4eecb43e7557b8
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2023-07-31 00:36:33 +0200

    Bump versions to v1.6.2

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -3,7 +3,7 @@ package core
 // version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.6.1"
+	Version = "1.6.2"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
--- utils/packaging/daemon/rpm/opensnitch.spec
+++ utils/packaging/daemon/rpm/opensnitch.spec
@@ -1,5 +1,5 @@
 Name:           opensnitch
-Version:        1.6.1
+Version:        1.6.2
 Release:        1%{?dist}
 Summary:        OpenSnitch is a GNU/Linux interactive application firewall
 
