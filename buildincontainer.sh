d765cf2ac7a589b8afa66045221ddb607c870d72
ozialien <ozialien@gmail.com>
2023-11-17 21:45:23 -0700

    added

--- buildincontainer.sh
+++ buildincontainer.sh
@@ -1,4 +1,4 @@
-go install google.golang.org/protobuf@latest
+go install google.golang.org/protobuf@3.20.x
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 export PATH=$PWD/bin:$PATH
