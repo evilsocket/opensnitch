f669c780b44aba7223d4f581abac114771669308
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2024-09-17 22:48:37 +0200

    ui: fixed syntax warning building appimages paths
    SyntaxWarning: invalid escape sequence '\.'
    
    (#1154)

--- ui/opensnitch/dialogs/prompt/_utils.py
+++ ui/opensnitch/dialogs/prompt/_utils.py
@@ -180,6 +180,6 @@ def get_combo_operator(data, comboText, con):
 
     elif data == _constants.FIELD_APPIMAGE:
         appimage_bin = os.path.basename(con.process_path)
-        appimage_path = os.path.dirname(con.process_path).replace(".", "\.")
+        appimage_path = os.path.dirname(con.process_path).replace('.', r'\.')
         appimage_path = appimage_path[0:len(_constants.APPIMAGE_PREFIX)+7]
         return Config.RULE_TYPE_REGEXP, Config.OPERAND_PROCESS_PATH, r'^{0}[0-9A-Za-z]{{6}}\/.*{1}$'.format(appimage_path, appimage_bin)
