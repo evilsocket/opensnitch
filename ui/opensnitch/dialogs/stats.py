f9e1a6a41fc513a58901dde17f42ab9fcbe3fdd1
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2024-12-27 16:32:21 +0100

    ui,stats: fixed displaying fw button icon
    In some distros like Ubuntu 25, we were not displaying the fw button
    icon, which made the button practically hidden to the user.
    
    We assumed that if 'document-new' icon was available, the rest of the
    icons would be available as well, so we wouldn't need to fallback to
    Qt's builtin icons.
    
    However on Ubuntu 25 for example, despite of 'document-new' being
    available, the icon 'security-high' was not being displayed when it was
    loaded from the stats.ui file.
    
    Loading it from code with QIcon.fromTheme() works fine, and even if
    it's not found (which sometimes happens), we'd fallback to Qt's builtin
    icons.
    
    Other icons may be optional, but icons without text need to have an
    icon always.

--- ui/opensnitch/dialogs/stats.py
+++ ui/opensnitch/dialogs/stats.py
@@ -698,13 +698,9 @@ class StatsDialog(QtWidgets.QDialog, uic.loadUiType(DIALOG_UI_PATH)[0]):
         return super(StatsDialog, self).eventFilter(source, event)
 
     def _configure_buttons_icons(self):
-        if QtGui.QIcon.hasThemeIcon("document-new"):
-            return
-
         newRuleIcon = Icons.new(self, "document-new")
         delRuleIcon = Icons.new(self, "edit-delete")
         editRuleIcon = Icons.new(self, "accessories-text-editor")
-        saveIcon = Icons.new(self, "document-save")
         prefsIcon = Icons.new(self, "preferences-system")
         searchIcon = Icons.new(self, "system-search")
         clearIcon = Icons.new(self, "edit-clear-all")
