970f8c356b356904e1abbc3d3af0b9fcda84a84a
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2025-09-03 17:45:33 +0200

    minor tasks notification change
    Simplify tasks' notifications handling.

--- daemon/ui/notifications_tasks.go
+++ daemon/ui/notifications_tasks.go
@@ -11,62 +11,47 @@ import (
 	"golang.org/x/net/context"
 )
 
+func (c *Client) dispatchTaskEvents(ctx context.Context, stream protocol.UI_NotificationsClient, notifId uint64, results <-chan interface{}, errors <-chan error) {
+	for {
+		select {
+		case <-ctx.Done():
+			goto Exit
+		case err := <-errors:
+			c.sendNotificationReply(stream, notifId, "", err)
+		case temp := <-results:
+			data, ok := temp.(string)
+			if !ok {
+				goto Exit
+			}
+			c.sendNotificationReply(stream, notifId, data, nil)
+		}
+	}
+Exit:
+	// task should have already been removed via TASK_STOP
+}
+
 func (c *Client) monitorSockets(config interface{}, stream protocol.UI_NotificationsClient, notification *protocol.Notification) {
 	sockMonTask, err := socketsmonitor.New(config, true)
 	if err != nil {
 		c.sendNotificationReply(stream, notification.Id, "", err)
 		return
 	}
-	ctxSock, err := TaskMgr.AddTask(socketsmonitor.Name, sockMonTask)
+	ctx, err := TaskMgr.AddTask(socketsmonitor.Name, sockMonTask)
 	if err != nil {
 		c.sendNotificationReply(stream, notification.Id, "", err)
 		return
 	}
-	go func(ctx context.Context) {
-		for {
-			select {
-			case <-ctx.Done():
-				goto Exit
-			case err := <-sockMonTask.Errors():
-				c.sendNotificationReply(stream, notification.Id, "", err)
-			case temp := <-sockMonTask.Results():
-				data, ok := temp.(string)
-				if !ok {
-					goto Exit
-				}
-				c.sendNotificationReply(stream, notification.Id, data, nil)
-			}
-		}
-	Exit:
-		// task should have already been removed via TASK_STOP
-	}(ctxSock)
+	go c.dispatchTaskEvents(ctx, stream, notification.Id, sockMonTask.Results(), sockMonTask.Errors())
 }
 
 func (c *Client) monitorNode(node, interval string, stream protocol.UI_NotificationsClient, notification *protocol.Notification) {
 	taskName, nodeMonTask := nodemonitor.New(node, interval, true)
-	ctxNode, err := TaskMgr.AddTask(taskName, nodeMonTask)
+	ctx, err := TaskMgr.AddTask(taskName, nodeMonTask)
 	if err != nil {
 		c.sendNotificationReply(stream, notification.Id, "", err)
 		return
 	}
-	go func(ctx context.Context) {
-		for {
-			select {
-			case <-ctx.Done():
-				goto Exit
-			case err := <-nodeMonTask.Errors():
-				c.sendNotificationReply(stream, notification.Id, "", err)
-			case temp := <-nodeMonTask.Results():
-				data, ok := temp.(string)
-				if !ok {
-					goto Exit
-				}
-				c.sendNotificationReply(stream, notification.Id, data, nil)
-			}
-		}
-	Exit:
-		TaskMgr.RemoveTask(taskName)
-	}(ctxNode)
+	go c.dispatchTaskEvents(ctx, stream, notification.Id, nodeMonTask.Results(), nodeMonTask.Errors())
 }
 
 func (c *Client) monitorProcessDetails(pid int, interval string, stream protocol.UI_NotificationsClient, notification *protocol.Notification) {
@@ -81,23 +66,5 @@ func (c *Client) monitorProcessDetails(pid int, interval string, stream protocol
 		c.sendNotificationReply(stream, notification.Id, "", err)
 		return
 	}
-	go func(ctx context.Context) {
-		for {
-			select {
-			case <-ctx.Done():
-				goto Exit
-			case err := <-pidMonTask.Errors():
-				c.sendNotificationReply(stream, notification.Id, "", err)
-			case temp := <-pidMonTask.Results():
-				data, ok := temp.(string)
-				if !ok {
-					goto Exit
-				}
-				c.sendNotificationReply(stream, notification.Id, data, nil)
-			}
-		}
-	Exit:
-		TaskMgr.RemoveTask(taskName)
-	}(ctx)
-
+	go c.dispatchTaskEvents(ctx, stream, notification.Id, pidMonTask.Results(), pidMonTask.Errors())
 }
