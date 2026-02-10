f1e5ba94fa5bde6766d261f75dbf4c15aa9a9463
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2026-02-10 23:05:01 +0100

    Bumped version to v1.9.0

--- daemon/core/version.go
+++ daemon/core/version.go
@@ -3,7 +3,7 @@ package core
 // version related consts
 const (
 	Name    = "opensnitch-daemon"
-	Version = "1.8.0"
+	Version = "1.9.0"
 	Author  = "Simone 'evilsocket' Margaritelli"
 	Website = "https://github.com/evilsocket/opensnitch"
 )
